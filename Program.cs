469890a93b9c5eb9f6edc4b3447f3c9d5fa2fca2
Denis <Dionisi2008@gmail.com>
2017-10-29 03:53:55 +0700

    new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ease_FileDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
