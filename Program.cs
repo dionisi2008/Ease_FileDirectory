ecb7460ba8925c68310cd2c900d52298fc75598d
Denis <dionisi2008@gmail.com>
2017-11-04 05:08:12 +0700

    что то про фаил открылся и все увидили

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace Ease_FileDirectory
     {
         static void Main(string[] args)
         {
+            if (args.Length != 0)
+            {
+                if (File.Exists(args[0]) == true)
+                {
+                EaseFileCompact ReadFile = new EaseFileCompact(args[0]);
+                string[] ReadListFiles = ReadFile.GetListFiles();
+                for (int shag = 0; shag <= ReadListFiles.Length - 1; shag++)
+                    {
+                    System.Console.WriteLine(ReadListFiles[shag]);
+                    }
+                }
+            }
             
         }
     }
